12589d3b37f0d5119243cc5815c2909534ab7376
Kęstutis Kuncė <kestutis.kunce@storebrand.no>
2018-10-19 17:04:41 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require("express");
+const path = require("path");
+
+const app = new express();
+
+require('dotenv').load();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use("*", function (req, resp) {
+    resp.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log("Application started");
+    console.log("Running on port: " + PORT);
+    console.log();
+});
