acc1b4283dbe658beb31b9940ec379a6c4ed8b65
Kestutis <kestutis.kunce@gmail.com>
2018-10-21 15:07:53 +0300

    Mobile header

--- src/components/partials/NavMenu.vue
+++ src/components/partials/NavMenu.vue
@@ -1,27 +1,44 @@
 <template>
-  <el-menu mode="horizontal">
-    <el-menu-item index="1">
+<div>
+  <div class="menu-icon" @click="onMenuClick()"><i class="el-icon-menu"></i></div>
+  <el-menu v-bind:class="{open: menuOpen}" mode="horizontal">
+    <el-menu-item class="menu-mobile-item" index="1" @click="closeMenu()">
       <router-link :to="{name: 'Dashboard'}">Pagrindinis</router-link>
     </el-menu-item>
-    <el-menu-item index="3">
+    <el-menu-item class="menu-mobile-item" index="3" @click="closeMenu()">
       <router-link :to="{name: 'TrainerItem'}">Treniruokliai</router-link>
     </el-menu-item>
-    <el-menu-item index="4">
+    <el-menu-item class="menu-mobile-item" index="4" @click="closeMenu()">
       <router-link :to="{name: 'TrainerGroup'}">Raumenų grupės</router-link>
     </el-menu-item>
   </el-menu>
+</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      menuOpen: false
+    }
+  },
+  methods : {
+    onMenuClick(){
+      this.menuOpen = !this.menuOpen;
+    },
+    closeMenu(){
+       this.menuOpen = false;
     }
   }
 }
 </script>
 
 <style scoped>
+.menu-icon{
+  padding: 5px;
+  font-size: 34px;
+  display: none;
+}
 .el-menu {
   padding-left: 1rem;
   padding-right: 1rem;
@@ -43,5 +60,27 @@ export default {
 .el-menu a.active {
   color: var(--gray);
   border-bottom: 1px solid var(--gray);
+}
+
+@media screen and (max-width: 768px){
+  .el-menu{
+    height: 0px;
+    overflow: hidden;
+    transition: height 0.5s;
+  }
+  .el-menu.open{
+    height: 180px;
+    overflow: hidden;
+  }
+  .menu-icon{
+    padding: 5px;
+    font-size: 34px;
+    display: block;
+    cursor: pointer;
+  }
+  li.menu-mobile-item{
+    display: block;
+    width: 100%;
+  }
 }
 </style>
